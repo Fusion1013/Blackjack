588835b3a8ebe71297e1cb5e4a72c0f46704b9a6
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2019-09-21 11:40:31 +0200

    Update Blackjack.hs

--- Blackjack.hs
+++ Blackjack.hs
@@ -3,27 +3,6 @@ import Cards
 import RunGame
 import Test.QuickCheck hiding (shuffle)
 
--- Example Card 1
-aCard1 :: Card
-aCard1 = Card (Numeric 2) Hearts
-
--- Example Card 2
-aCard2 :: Card
-aCard2 = Card Jack Hearts
-
-aCard3 :: Card
-aCard3 = Card Ace Hearts
-
--- Example Hand
-aHand :: Hand
-aHand = [aCard1, aCard2]
-
-aHand2 :: Hand
-aHand2 = [aCard3, aCard3, aCard2]
-
-aDeck :: Deck
-aDeck = [aCard3, aCard3, aCard2]
-
 -- Prints the given hand
 display :: Hand -> String
 display [] = ""
@@ -41,8 +20,8 @@ formatRank rank = show rank
 -- Returns the value of an entire hand
 value :: Hand -> Int
 value hand | rest > 10 = rest + aces
-           | aces >= 2 = rest + aces
-           | otherwise = rest + (aces * 11)
+            | aces >= 2 = rest + aces
+            | otherwise = rest + (aces * 11)
   where -- Saves the values to simplify the function above
     aces = numberOfAces hand
     rest = sum (map valueCard hand)
@@ -55,7 +34,7 @@ valueCard card = valueRank (rank card)
 valueRank :: Rank -> Int
 valueRank (Numeric n) = n
 valueRank rank | rank == Ace = 0 -- Aces are added in the "value" function
-               | otherwise = 10
+                | otherwise = 10
 
 -- Counts the number of aces in a given hand
 numberOfAces :: Hand -> Int
@@ -74,18 +53,17 @@ gameOver hand | value hand > 21 = True
 -- Returns the winner given the two hands
 winner :: Hand -> Hand -> Player
 winner guestHand hand | value guestHand <= value hand = Bank
-                          | value guestHand > 21 = Bank
-                          | otherwise = Guest
+                      | value guestHand > 21 = Bank
+                      | otherwise = Guest
 
 --- TASK B1 ---
-
 -- A list containing all possible cards
 fullDeck :: Deck
 fullDeck = [Card x y | x <- allRanks, y <- allSuits]
 
 -- Returns a list of all ranks
 allRanks :: [Rank]
-allRanks = Jack : (King : (Queen : (Ace : [(Numeric x) | x<-[2..10]])))
+allRanks = [Jack, King, Queen, Ace] ++ [(Numeric x) | x<-[2..10]]
 
 -- Returns a list of all suits
 allSuits :: [Suit]
@@ -96,14 +74,12 @@ prop_size_fullDeck :: Bool
 prop_size_fullDeck = size fullDeck == 52
 
 --- TASK B2 ---
-
 -- Draws a card from the deck and adds it to the hand
 draw :: Deck -> Hand -> (Deck,Hand)
 draw [] _ = error "draw: The deck is empty"
-draw (x:xs) hand = (xs, x : hand) -- ELEMENT : [SAKER] = [ELEMENT, SAKER]
+draw (x:xs) hand = (xs, x : hand)
 
 --- TASK B3 ---
-
 -- Plays the bank
 playBank :: Deck -> Hand
 playBank deck = bankCards (deck, [])
